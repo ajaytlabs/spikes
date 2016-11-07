81df35bd3ab0818b5db74086f9425a50b508a9d2
Ryan Feline <ryan.feline@novoda.com>
2016-11-07 19:07:46 +0000

    Add response text to the app.

--- TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/TpBotActivity.java
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/TpBotActivity.java
@@ -9,5 +9,7 @@ public class TpBotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tp_bot);
+
+        final TextView responseText = (TextView) findViewById(R.id.response);
     }
 }
