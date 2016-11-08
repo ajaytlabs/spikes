5a00eab92e750db682816034c4f3c499d6de53fd
Ryan Feline <ryan.feline@novoda.com>
2016-11-08 18:54:45 +0000

    Toast a message when connecting to socket io server.

--- TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/TpBotActivity.java
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/TpBotActivity.java
@@ -1,13 +1,53 @@
 package com.novoda.tpbot;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class TpBotActivity extends AppCompatActivity {
+
+    private Socket socket;
+
+    {
+        try {
+            socket = IO.socket("http://192.168.86.152:3000");
+        } catch (URISyntaxException e) {
+            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tp_bot);
+
+        socket.on("CONNECTED", onConnected);
+        socket.connect();
+    }
+
+    private Emitter.Listener onConnected = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(TpBotActivity.this, "User connected", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        socket.disconnect();
+        socket.off("CONNECTED", onConnected);
     }
 }
