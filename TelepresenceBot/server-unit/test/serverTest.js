6b1103fe510d9b6986a40035679b5b46c7c652a3
Ryan Feline <ryan.feline@novoda.com>
2017-02-03 15:38:37 +0000

    Fix disconnection issue in tests.

--- TelepresenceBot/server-unit/test/serverTest.js
+++ TelepresenceBot/server-unit/test/serverTest.js
@@ -11,26 +11,44 @@ var options ={
 
 describe("TelepresenceBot Server",function() {
 
-    it('Should add new bot to list of bots', function(done) {
+    it('Should add new bot to list of bots.', function(done) {
         var bot = io.connect(socketURL, options);
 
         bot.emit('use_test_socket');
 
         bot.on('connect', function(data) {
-            console.log('test bot connected');
-
             bot.emit('connect_as_bot', bot.id, callback);
         });
 
         var callback = function(text) {
-            console.log('callback received');
-
             var expectedBots = [bot.id];
 
             test.array(expectedBots)
                 .is(text);
 
-            bot.disconnect;
+            bot.disconnect();
+            done();
+        }
+
+    });
+
+    it('Should ignore multiple connections from same bot.', function(done) {
+        var bot = io.connect(socketURL, options);
+
+        bot.emit('use_test_socket');
+
+        bot.on('connect', function(data) {
+            bot.emit('connect_as_bot', bot.id, callback);
+            bot.emit('connect_as_bot', bot.id, callback);
+        });
+
+        var callback = function(text) {
+            var expectedBots = [bot.id];
+
+            test.array(expectedBots)
+                .is(text);
+
+            bot.disconnect();
             done();
         }
 
