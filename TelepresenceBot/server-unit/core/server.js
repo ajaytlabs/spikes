9891404abae31a797eff3912f36addd751a10bbf
Ryan Feline <ryan.feline@novoda.com>
2017-02-15 09:26:26 +0000

    Check for existance before removing client from lists.

--- TelepresenceBot/server-unit/core/server.js
+++ TelepresenceBot/server-unit/core/server.js
@@ -27,6 +27,7 @@ io.sockets.on('connection', function (client) {
 
     client.on('disconnect', function() {
         disconnectBot(client);
+        disconnectHuman(client);
     });
 
 });
@@ -47,22 +48,32 @@ var determineCallback = function(callback, clients) {
     }
 }
 
-var connectHuman = function(client, callback) {
-    humanClients[client.id] = client;
-    determineCallback(callback, humanClients);
-    console.log('Human connected: ' + client.id);
-}
-
-var disconnectBot = function(client, callback) {
-    delete botClients[client.id];
-    determineCallback(callback, botClients);
-    console.log('Bot disconnected: ' + client.id);
-}
-
 var toKeysArrayFrom = function(objects) {
     return Object.keys(objects).map(
         function(value) {
             return value;
         }
     );
+}
+
+var connectHuman = function(client, callback) {
+    humanClients[client.id] = client;
+    determineCallback(callback, humanClients);
+    console.log('Human connected: ' + client.id);
+}
+
+var disconnectHuman = function(client, callback) {
+    if(humanClients[client.id] != undefined) {
+        delete humanClients[client.id];
+        determineCallback(callback, humanClients);
+        console.log('Human disconnected: ' + client.id);
+    }
+}
+
+var disconnectBot = function(client, callback) {
+    if(botClients[client.id] != undefined) {
+        delete botClients[client.id];
+        determineCallback(callback, botClients);
+        console.log('Bot disconnected: ' + client.id);
+    }
 }
