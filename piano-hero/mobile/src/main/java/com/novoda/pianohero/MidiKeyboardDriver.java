dbf8dcf5ade3ee75fa77610c97f18cc30eddd0aa
Paul Blundell <paul@novoda.com>
2017-05-27 11:21:54 +0100

    callback on mainthread

--- piano-hero/mobile/src/main/java/com/novoda/pianohero/MidiKeyboardDriver.java
+++ piano-hero/mobile/src/main/java/com/novoda/pianohero/MidiKeyboardDriver.java
@@ -1,6 +1,7 @@
 package com.novoda.pianohero;
 
 import android.content.Context;
+import android.os.Handler;
 
 import jp.kshoji.driver.midi.device.MidiInputDevice;
 
@@ -13,10 +14,14 @@ interface MidiKeyboardDriver {
     void close();
 
     class KeyStationMini32 extends SimpleUsbMidiDriver implements MidiKeyboardDriver {
+
+        private final Handler mainThreadHandler;
+
         private NoteListener noteListener;
 
         KeyStationMini32(Context context) {
             super(context);
+            mainThreadHandler = new Handler(context.getMainLooper());
         }
 
         @Override
@@ -26,26 +31,38 @@ interface MidiKeyboardDriver {
 
         @Override
         public void onMidiNoteOn(
-                MidiInputDevice midiInputDevice,
-                int cable,
-                int channel,
-                int note,
-                int velocity) {
-            if (noteListener != null) {
-                noteListener.onPress(new Note(note));
+            MidiInputDevice midiInputDevice,
+            int cable,
+            int channel,
+            final int note,
+            int velocity) {
+            if (noteListener == null) {
+                return;
             }
+            mainThreadHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    noteListener.onPress(new Note(note));
+                }
+            });
         }
 
         @Override
         public void onMidiNoteOff(
-                MidiInputDevice midiInputDevice,
-                int cable,
-                int channel,
-                int note,
-                int velocity) {
-            if (noteListener != null) {
-                noteListener.onRelease(new Note(note));
+            MidiInputDevice midiInputDevice,
+            int cable,
+            int channel,
+            final int note,
+            int velocity) {
+            if (noteListener == null) {
+                return;
             }
+            mainThreadHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    noteListener.onRelease(new Note(note));
+                }
+            });
         }
     }
 }
